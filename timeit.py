c400e91fe799ae11f76557c24bd7d688e5978ca9
Hieu Nguyen <hieutrinhnguyen1@gmail.com>
2022-03-05 16:39:36 -0800

    "edit#1"

--- timeit.py
+++ timeit.py
@@ -5,12 +5,8 @@ def calculate_time(func):
 		start = time.time()
 		func()
 		end = time.time()
-		print("Total time {end-start}")
+		print(f"Total time {end-start}")
 	return wrapper
 
 
-@calculate_time
-def hello():
-	print("Hello world!")
 
-calculate_time(hello)
