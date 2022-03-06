2f51d4bd1a355578814d568b8d719798651bd43a
Hieu Nguyen <hieutrinhnguyen1@gmail.com>
2022-03-05 17:39:10 -0800

    "add file"

---
+++ document_analyzer.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+with open('document.txt') as f:
+	lines = f.read()
+	
+words = lines.split()
+count = Counter(words)
+top5 = count.most_common(5)
+
+
+
+for pair in top5:
+	string = ''
+	for item in pair:
+		item_isInt = isinstance(item, int)
+		if(item_isInt):
+			string = string + " : " + str(item)
+			print(string)
+		else:
+			string = item
